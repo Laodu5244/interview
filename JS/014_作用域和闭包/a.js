3caa605e6b67b4c00b3ccb8c04df7b1ce02508f4
laodu <laodu5244@163.com>
2022-09-11 16:13:14 +0800

    'add'

---
+++ JS/014_作用域和闭包/a.js
@@ -0,0 +1,9 @@
+var a = (function(){
+	var n = 2000
+	var m = 1000
+
+	function sub(){
+		console.log(n-m)
+	}
+	return sub
+})()
---
+++ JS/014_作用域和闭包/b.js
@@ -0,0 +1,8 @@
+var b = (function(){
+	var n = 10
+	var m = 20
+	function add(){
+		console.log(n+m)
+	}
+	return add
+})()
